c6a87fda1f79e30e12e94266f12a3764035af30b
Chigozie Peter <ujahchigozie@gmail.com>
2022-05-08 03:06:54 +0800

    Update Service.php

--- src/Service.php
+++ src/Service.php
@@ -1,6 +1,6 @@
 <?php 
 /**
- * OBCompress - A simple php class to compress output buffer
+ * Service - Send firebase device push notification from php curl
  * @author      Peter Chigozie(NG) peterujah
  * @copyright   Copyright (c), 2021 Peter(NG) peterujah
  * @license     MIT public license
@@ -8,24 +8,64 @@
 namespace Peterujah\NanoBlock\Firebase;
 use \Peterujah\NanoBlock\Firebase\ServiceModel;
  class Service {
-    public const SINGLE = "single";
-    public const GROUP = "multiple";
-    protected $api_kay;
-    protected $endpoint =  'https://fcm.googleapis.com/fcm/send';
+	/** 
+	* Holds notification broadcast type [single]
+	* @static string 
+	*/
+	public const SINGLE = "single";
 
-    public function __construct($key) {
-        $this->api_kay = $key;
-    }
+	/** 
+	* Holds notification broadcast type [multiple]
+	* @static string 
+	*/
+	public const GROUP = "multiple";
 
-    public function notifyAll(ServiceModel $payload){
-        return $this->send((array) $payload->get(), self::GROUP);
-    }
+	/** 
+	* Holds google firebase notification api key
+	* @static string 
+	*/
+	protected $api_kay;
 
-    public function notify(ServiceModel $payload){
-        return $this->send((array) $payload->get(), self::SINGLE);
-    }
+	/** 
+	* Holds firebase notification endpoint
+	* @static string 
+	*/
+	protected $endpoint =  'https://fcm.googleapis.com/fcm/send';
 
-	private function send($sendFields, $type) {
+
+	/** 
+	* Constructor
+	* @param string $key google firebase service key 
+	*/
+	public function __construct($key) {
+		$this->api_kay = $key;
+	}
+
+	/** 
+	* Sends notification to all users
+	* @param ServiceModel $payload
+	* @return response|object|array response from google firebase service
+	*/
+	public function notifyAll(ServiceModel $payload){
+		return $this->send((array) $payload->get(), self::GROUP);
+	}
+
+	/** 
+	* Sends notification to a single user
+	* @param ServiceModel $payload
+	* @return response|object|array response from google firebase service
+	*/
+	public function notify(ServiceModel $payload){
+		return $this->send((array) $payload->get(), self::SINGLE);
+	}
+
+	/** 
+	* Sends notification network request to google firebase api 
+	* @param array $payload
+	* @param string $type
+	* @return response|object|array response from google firebase service
+	*/
+	private function send($payload, $type) {
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, $this->endpoint);
 		curl_setopt($ch, CURLOPT_POST, true);
@@ -36,7 +76,7 @@ use \Peterujah\NanoBlock\Firebase\ServiceModel;
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);   
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-		curl_setopt($ch, CURLOPT_POSTFIELDS,  json_encode($sendFields));
+		curl_setopt($ch, CURLOPT_POSTFIELDS,  json_encode($payload));
 		$result = curl_exec($ch);        
 		if ($result === false) {
 			return 'FirebaseNotificationError: ' . curl_error($ch);
